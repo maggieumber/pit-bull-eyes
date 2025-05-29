819d1d8710d0bff9741683a38fd7962a8b44078d
maggieumber@gmail.com <maggieumber@gmail.com>
2025-05-29 15:31:56 -0500

    add code to make music play

--- sketch.js
+++ sketch.js
@@ -1,8 +1,23 @@
 var song;
 var myFont;
 
+function preload() {
+  // "SCP-x2x (Unseen Presence)" Kevin MacLeod (incompetech.com)
+    // Licensed under Creative Commons: By Attribution 4.0 License
+    // http://creativecommons.org/licenses/by/4.0/
+  soundFormats('mp3', 'ogg');
+  song = loadSound('assets/SCP-x2x.mp3');
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  cnv = createCanvas(windowWidth, windowHeight);
+  cnv.mousePressed(canvasPressed); 
+}
+
+function canvasPressed() {
+  // playing a sound file on a user gesture
+  // is equivalent to `userStartAudio()`
+  song.play();
 }
 
 function draw() {
